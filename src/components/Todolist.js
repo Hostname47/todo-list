fb90f78796cdccb589e9777e47e7fc3af0c1b763
HOSTNAME47 <mouadstev1@gmail.com>
2022-09-09 23:14:52 +0100

    Handle todolist refresh

--- src/components/Todolist.js
+++ src/components/Todolist.js
@@ -1,55 +1,39 @@
-import React, { useContext, useEffect } from 'react'
+import React, { useEffect } from 'react'
 import Task from './Task'
-import { TodolistContext } from '../App'
+import { connect } from 'react-redux'
+import { fetchTodolist } from '../redux/todolist/todolistActions'
 
-function Todolist() {
-  const { state, dispatch } = useContext(TodolistContext)
+function Todolist({ todolist, fetchTasks }) {
   /**
    * Fetch todo list from IndexedDB; but for now let's just pretend :)
    */
   useEffect(() => {
-    const request = indexedDB.open('todos', 1)
-    request.onerror = function (event) {
-      // messagesContext.dispatch({ type: 'error', value: 'Tasks fetching went wrong for some reason' })
-    };
-    request.onupgradeneeded = function () {
-      const db = request.result;
-      const store = db.createObjectStore("tasks", { keyPath: "id", autoIncrement: true });
-      store.createIndex("done_tasks", ["done"], { unique: false });
-    };
-    request.onsuccess = function() {
-      const db = request.result;
-      const transaction = db.transaction("tasks", 'readonly');
-
-      const store = transaction.objectStore("tasks");
-      const dataRequest = store.getAll()
-
-      dataRequest.onsuccess = () => {
-        // Fill returned data to todolist (and set loading to false)
-        dispatch({ type: 'fillTasks', tasks: dataRequest.result })
-      }
-
-      transaction.oncomplete = () => {
-        db.close()
-      }
-    }
-  }, [state.refresh])
+    fetchTasks()
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
 
   console.log('--- Render Todolist ---')
   return (
     <div>
       {
-        state.loading
+        todolist.loading 
           ?
-            <div className='full-center align-center g6 my8'>
+            <div className='full-center align-center g6' style={{ marginTop: '26px' }}>
               <svg className="spinner size14 rotate" fill="none" viewBox="0 0 16 16"><circle cx="8" cy="8" r="7" stroke="currentColor" strokeOpacity="0.25" strokeWidth="2" vectorEffect="non-scaling-stroke"></circle><path d="M15 8a7.002 7.002 0 00-7-7" stroke="currentColor" strokeWidth="2" strokeLinecap="round" vectorEffect="non-scaling-stroke"></path></svg>
               <span>fetching tasks..</span>
             </div>
-          : 
+          :
+          todolist.error 
+          ?
+            <div className='text-info-section error'>
+              <svg className="icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 260 260"><path d="M129,233.33h-108c-13.79,0-18.82-8.86-11.87-20.89q54-93.6,108.12-187.2c7.34-12.71,17.14-12.64,24.55.17,36,62.4,71.95,124.88,108.27,187.13,7.05,12.07-.9,21.28-12.37,21.06C201.43,232.88,165.21,233.33,129,233.33Zm91.36-24L129.4,51.8,38.5,209.3Zm-79-103.77c-.13-7.56-5.28-13-12-12.85s-11.77,5.58-11.82,13.1q-.13,20.58,0,41.18c.05,7.68,4.94,13,11.69,13.14,6.92.09,12-5.48,12.15-13.39.09-6.76,0-13.53,0-20.29C141.35,119.45,141.45,112.49,141.32,105.53Zm-.15,70.06a12.33,12.33,0,0,0-10.82-10.26,11.29,11.29,0,0,0-12,7.71,22.1,22.1,0,0,0,0,14A11.82,11.82,0,0,0,131.4,195c6.53-1.09,9.95-6.11,9.81-14.63A31.21,31.21,0,0,0,141.17,175.59Z"></path></svg>
+              <span className='text'>{ todolist.error }</span>
+            </div>
+          :
             <div id='todo-items-box'>
-              { state.tasks.length
-                ? state.tasks.map(task => (
-                    <Task key={ task.id } task={ task } todolistDispatch={ dispatch } />
+              { todolist.tasks.length
+                ? todolist.tasks.map(task => (
+                    <Task key={ task.id } task={ task } />
                   )
                 )
                 : (
@@ -65,4 +49,16 @@ function Todolist() {
   )
 }
 
-export default Todolist
+const mapStateToProps = state => {
+  return {
+    todolist: state.todolist
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    fetchTasks: () => dispatch(fetchTodolist())
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Todolist)
--- src/redux/todolist/todolistActions.js
+++ src/redux/todolist/todolistActions.js
@@ -19,3 +19,35 @@ export const fetchTodolistFailure = error => {
     payload: error
   }
 }
+
+export const fetchTodolist = () => {
+  return dispatch => {
+    // Dispatch fetch tasks request action
+    dispatch(fetchTodolistRequest())
+
+    const request = indexedDB.open('todos', 1)
+    request.onerror = function (event) {
+      // Dispatch failure action to show error
+      dispatch(fetchTodolistFailure('Tasks fetching went wrong for some reason'))
+    };
+    request.onupgradeneeded = function () {
+      const db = request.result;
+      db.createObjectStore("tasks", { keyPath: "id", autoIncrement: true });
+    };
+    request.onsuccess = function() {
+      const db = request.result;
+      const transaction = db.transaction("tasks", 'readonly');
+      const store = transaction.objectStore("tasks");
+      const dataRequest = store.getAll()
+
+      dataRequest.onsuccess = () => {
+        // Dispatch success action to fill tasks
+        dispatch(fetchTodolistSuccess(dataRequest.result))
+      }
+
+      transaction.oncomplete = () => {
+        db.close()
+      }
+    }
+  }
+}
